95201db0e72763f8192cd15cdd6cf1616d22f837
ihallo <aastorres2001@gmail.com>
2025-02-26 10:07:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,9 +20,6 @@ from sklearn.metrics import accuracy_score, classification_report
 from sklearn.cluster import KMeans
 from sklearn.semi_supervised import LabelPropagation
 
-# Download latest version
-path = kagglehub.dataset_download("uom190346a/ai-powered-job-market-insights")
-
 print("Path to dataset files:", path)
 
 #List the files in the downloaded dataset directory
@@ -30,13 +27,13 @@ files = os.listdir(path)
 print("Files in the downloaded dataset directory:", files)
 
 # Load the dataset (make sure to replace 'weather_data.csv' with the actual file name)
-file_name = 'ai_job_market_insights.csv'  # Adjust as necessary
+file_name = 'Balanced_CoffeeDS_500.csv'  # Adjust as necessary
 file_path = os.path.join(path, file_name)
 
 # Read the CSV file
 df = pd.read_csv(file_path)
 
-st.title("Job Market Insights")
+st.title("Coffee Dataset")
 
 st.subheader("Dataset Preview")
 st.write(df.head())
@@ -45,16 +42,17 @@ st.write(df.head())
 st.subheader("Column Descriptions")
 
 column_descriptions = {
-    "Job_Title": "The title of the job position.",
-    "Industry": "The type of industry that is hiring.",
-    "Company_Size": "The company size of the job.",
-    "Location": "The location of the job.",
-    "AI_Adoption_Level": "The level to which the company has integrated AI into its operations.",
-    "Automation_Risk": "The probability that the job will be automated during the next years.",
-    "Required_Skills": "The primary skills required for the job role.",
-    "Salary_USD": "The job offers an annual pay in USD.",
-    "Remote_Friendly": "Determines whether the job may be completed remotely.",
-    "Job_Growth_Projection": "The expected growth or drop in the job position over the following five years."
+    "Coffee Name": "The name of the coffee drink.",
+    "Caffeine Level": "The level of caffeine present in the coffee (e.g., Low, Medium, High).",
+    "Sweetness": "The sweetness level of the coffee (e.g., Low, Medium, High).",
+    "Type": "Specifies whether the coffee is Iced, Hot, or Frozen.",
+    "Roast Level": "The roast level of the coffee beans used (e.g., Light, Medium, Dark).",
+    "Milk Type": "The type of milk used in the coffee (e.g., Dairy, Almond, None).",
+    "Flavor Notes": "The primary flavor notes of the coffee (e.g., Chocolate, Vanilla, Caramel).",
+    "Acidity Level": "The level of acidity in the coffee (e.g., Low, Medium, High).",
+    "Bitterness Level": "The level of bitterness in the coffee (e.g., Low, Medium, High).",
+    "Weather": "The ideal weather condition for consuming the coffee (e.g., Hot, Cold).",
+    "Temperature": "The temperature preference for the coffee (e.g., Hot, Iced, Frozen)."
 }
 
 for column, description in column_descriptions.items():
@@ -66,17 +64,11 @@ df.info(buf=buffer)
 info = buffer.getvalue()  
 st.text(info)
 
-st.write("### **Observation**")
-st.write("As we can see from the DataFrame information, there are no null values in any of the data types. Based on this, we can conclude that there are no data columns that need to be dropped.")
-
 st.subheader("Duplicate Rows")
 st.write(f"Number of duplicate rows: {df.duplicated().sum()}")  
 
 duplicates = df[df.duplicated(keep=False)]
 st.dataframe(duplicates)
-
-st.write("### **Observation**")
-st.write("There are no duplicate rows found on the dataframe.")
 
 # Categorical columns
 cat_col = [col for col in df.columns if df[col].dtype == 'object']
@@ -103,74 +95,44 @@ for col in cat_col:
     
 for col in num_col:
     st.write(f'Unique values in {col}:', df[col].unique())
-    
-st.write("### **Observation**")
-st.write("We used df[cat_col].nunique() to identify how many unique data in each categorized columns. Since there is only one numerical which is Salary_USD and we identified that all categorical columns are properly intialized with the use of .unique().")
 
 df_data = pd.DataFrame()
 
-job_encoder = LabelEncoder()
-industry_encoder = LabelEncoder()
-size_encoder = LabelEncoder()
-location_encoder = LabelEncoder()
-ai_adoption_encoder = LabelEncoder()
-automation_encoder = LabelEncoder()
-skills_encoder = LabelEncoder()
-remote_encoder = LabelEncoder()
-growth_encoder = LabelEncoder()
+coffee_name_encoder = LabelEncoder()
+caffeine_encoder = LabelEncoder()
+sweetness_encoder = LabelEncoder()
+type_encoder = LabelEncoder()
+roast_encoder = LabelEncoder()
+milk_encoder = LabelEncoder()
+flavor_encoder = LabelEncoder()
+acidity_encoder = LabelEncoder()
+bitterness_encoder = LabelEncoder()
+weather_encoder = LabelEncoder()
+temperature_encoder = LabelEncoder()
 
-df_data['Job_encoded'] = job_encoder.fit_transform(df['Job_Title'])
-df_data['Industry_encoded'] = industry_encoder.fit_transform(df['Industry'])
-df_data['Size_encoded'] = size_encoder.fit_transform(df['Company_Size'])
-df_data['Location_encoded'] = location_encoder.fit_transform(df['Location'])
-df_data['AI_Adoption_encoded'] = ai_adoption_encoder.fit_transform(df['AI_Adoption_Level'])
-df_data['Automation_encoded'] = automation_encoder.fit_transform(df['Automation_Risk'])
-df_data['Skills_encoded'] = skills_encoder.fit_transform(df['Required_Skills'])
-df_data['Remote_encoded'] = remote_encoder.fit_transform(df['Remote_Friendly'])
-df_data['Growth_encoded'] = growth_encoder.fit_transform(df['Job_Growth_Projection'])
-df_data['Salary_USD'] = df['Salary_USD']
+df_data['Coffee_encoded'] = coffee_name_encoder.fit_transform(df['Coffee Name'])
+df_data['Caffeine_encoded'] = caffeine_encoder.fit_transform(df['Caffeine Level'])
+df_data['Sweetness_encoded'] = sweetness_encoder.fit_transform(df['Sweetness'])
+df_data['Type_encoded'] = type_encoder.fit_transform(df['Type'])
+df_data['Roast_encoded'] = roast_encoder.fit_transform(df['Roast Level'])
+df_data['Milk_encoded'] = milk_encoder.fit_transform(df['Milk Type'])
+df_data['Flavor_encoded'] = flavor_encoder.fit_transform(df['Flavor Notes'])
+df_data['Acidity_encoded'] = acidity_encoder.fit_transform(df['Acidity Level'])
+df_data['Bitterness_encoded'] = bitterness_encoder.fit_transform(df['Bitterness Level'])
+df_data['Weather_encoded'] = weather_encoder.fit_transform(df['Weather'])
+df_data['Temperature_encoded'] = temperature_encoder.fit_transform(df['Temperature'])
 
-st.subheader('Encoding Process:')
-st.code("""
-job_encoder = LabelEncoder() 
-industry_encoder = LabelEncoder()
-size_encoder = LabelEncoder()
-location_encoder = LabelEncoder()
-ai_adoption_encoder = LabelEncoder()
-automation_encoder = LabelEncoder()
-skills_encoder = LabelEncoder()
-remote_encoder = LabelEncoder()
-growth_encoder = LabelEncoder()
+st.write("# Prediction")
 
-df_data['Job_encoded'] = job_encoder.fit_transform(df['Job_Title'])
-df_data['Industry_encoded'] = industry_encoder.fit_transform(df['Industry'])
-df_data['Size_encoded'] = size_encoder.fit_transform(df['Company_Size'])
-df_data['Location_encoded'] = location_encoder.fit_transform(df['Location'])
-df_data['AI_Adoption_encoded'] = ai_adoption_encoder.fit_transform(df['AI_Adoption_Level'])
-df_data['Automation_encoded'] = automation_encoder.fit_transform(df['Automation_Risk'])
-df_data['Skills_encoded'] = skills_encoder.fit_transform(df['Required_Skills'])
-df_data['Remote_encoded'] = remote_encoder.fit_transform(df['Remote_Friendly'])
-df_data['Growth_encoded'] = growth_encoder.fit_transform(df['Job_Growth_Projection'])
-df_data['Salary_USD'] = df['Salary_USD']
-""")
-
-st.write("### **Observation**")
-st.write(" We encoded all object datatypes for RandomForestClassifier.", "We also created a new DataFrame to store the encoded data.")
-
-st.write("# Automation Risk Prediction")
-
-features = ['Job_encoded', 'Industry_encoded', 'Size_encoded', 'Location_encoded', 'AI_Adoption_encoded','Skills_encoded', 'Remote_encoded', 'Growth_encoded','Salary_USD']
+features = ['Coffee_encoded', 'Caffeine_encoded', 'Sweetness_encoded', 'Type_encoded', 'Roast_encoded', 'Flavor_encoded', 'Milk_encoded', 'Weather_encoded', 'Temperature_encoded']
 X1 = df_data[features]
-Y1 = df_data['Automation_encoded']
+Y1 = df_data['Bitterness_encoded']
 
 st.subheader('Features (X1):')
 st.write(X1)
 
 st.subheader('Target Variable (Y1):')
 st.write(Y1)
-
-st.write("### **Observation**")
-st.write(" To prepare for machine learning we categorized the needed datatype for predicition inside X. While we put the data will be predicted to Y which is Automation_Risk.")
 
 # Min-Max Scaling (Normalization)
 
@@ -193,25 +155,11 @@ X1[num_col_] = scaler.fit_transform(X1[num_col_])
 st.write('Scaled Features (X1):')
 st.dataframe(X1)
 
-st.write("### **Observation**")
-st.write(" We used Min-Max scaling for standardization to help the machine learning. Since, standardization makes the data more suitable for algorithms that assume a Gaussian distributuion or require features to have zero mean and unit variance. As we can see now, the Salary_USD was converted to a decimal from a whole number.")
-
 st.subheader('Automation Risk Distribution')
 
-automationRisk_list = df['Automation_Risk'].unique().tolist()
-automationRisk_counts = df['Automation_Risk'].value_counts()
-automationRisk_counts_list = automationRisk_counts.tolist()
-
-def pie_chart_AutomationRisk():
-
- 
-  plt.pie(automationRisk_counts_list, labels=automationRisk_list, autopct='%1.1f%%')
-  plt.title('Pie Chart of Automation Risk')
-  st.pyplot(plt)
-pie_chart_AutomationRisk()
-
-st.write("### **Observation**")
-st.write("We created a piechart for our Automation Risk (our first Y) and we can see that it is almost evenly distributed. Which is good for our machine learning to not have any biases.")
+bitterness_list = df['Bitterness Level'].unique().tolist()
+bitterness_counts = df['Bitterness Level'].value_counts()
+bitterness_counts_list = bitterness_counts.tolist()
 
 X1_train, X1_test, Y1_train, Y1_test = train_test_split(X1, Y1, test_size=0.1, random_state=42)
 
@@ -229,15 +177,6 @@ st.dataframe(Y1_train)
 st.write('Testing Target Variable (Y1_test):')
 st.dataframe(Y1_test)
 
-st.write("### **Observation**")
-st.markdown("""
-*  We used `.head()` to give us a preview of the `X1_test` and `X1_train` data.
-*  We used `.shape` to give us the dimensions of the `X1_test` and `X1_train` data.
-* Same case with the Y1_train and Y1_test.
-
- As you can see, the test shape has a lower number than the training shape. Since we should always allot more data to training for the machine learning model to practice.
-""")
-
 clf = RandomForestClassifier(random_state=42)
 clf.fit(X1_train, Y1_train)
 
@@ -246,9 +185,6 @@ st.code("""
 * clf.fit(X1_train, Y1_train)""")
 
 st.write("RandomForestClassifier model trained successfully!")
-
-st.write("### **Observation**")
-st.write(" We used RandomForest Classifier algorithm for our supervised learning. To improve prediction accuracy and reduce overfitting.")
 
 st.markdown("#### **Model Evaluation**")
 
@@ -267,13 +203,12 @@ accuracy = accuracy_score(Y1_test, Y1_pred)
 st.write("### Accuracy:")
 st.markdown(f'{accuracy * 100:.2f}%')
 
-st.write("### **Observation**")
-st.write(" As we predict the Y and Based on the Performance evaluation - We can see the accuracy is low.")
-
 feature_importance = clf.feature_importances_
 st.code("""feature_importance = clf.feature_importances_""")
 st.write("### feature_importance:")
 st.write(feature_importance)
+
+# --------------------------------------------------------> Hanggang dito palang natatapos ko -Alex
 
 importance_df = pd.DataFrame({
     'Feature': X1.columns,
